5ec8e3f0166392456cc1dede801c5392d169ccdc
Tomek Niedźwiedź <tomasz.k.niedzwiedz@gmail.com>
2015-04-12 12:36:20 +0100

    Solution for P99_S31 - prime numbers

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P31.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P31.scala
@@ -2,6 +2,13 @@ package pl.japila.scalania.s99
 
 object S99_P31 {
   implicit class IntWithIsPrime(n: Int) {
-    def isPrime: Boolean = ???
+    def isPrime: Boolean = {
+      if(n < 2) {
+        false
+      } else {
+        val potentialDivisors = List.range(2, (n / 2))
+        potentialDivisors.takeWhile(divisor => n % divisor != 0) == potentialDivisors
+      }
+    }
   }
 }
